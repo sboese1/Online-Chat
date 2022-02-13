bee6ca65e35ca470165f2b4001f6f7b09d603ee0
sboese1 <91388561+sboese1@users.noreply.github.com>
2022-02-12 22:33:48 -0600

    Added comments to server and client files

--- client.py
+++ client.py
@@ -7,28 +7,28 @@ host = "127.0.0.1"
 port = 5002
 separator_token = '<SEP>'
 
-s = socket.socket()
+s = socket.socket() # Creates a new socket object
 print(f"[*] Connecting to {host}:{port}...")
-s.connect((host, port))
+s.connect((host, port)) # Connects to the remote socket at the given address
 print("[+] Connected.")
 
 name = input("Enter your name: ")
 
 def listen_for_messages():
     while True:
-        message = s.recv(1024).decode()
+        message = s.recv(1024).decode() # Receives data from sockets and decodes it
         print("\n" + message)
 
-t = Thread(target=listen_for_messages)
+t = Thread(target=listen_for_messages) # Creates a Thread object with the object being the function listen_for_messages
 t.daemon = True
 t.start()
 
 while True:
-    to_send =  input()
+    to_send = input()
     if to_send.lower() == 'q':
         break
-    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # Gets the current time
     to_send = f"[{date_now}] {name}{separator_token}{to_send}"
-    s.send(to_send.encode())
+    s.send(to_send.encode()) # Sends the encoded message
 
 s.close()
--- server.py
+++ server.py
@@ -6,9 +6,9 @@ port = 5002
 separator_token = '<SEP>'
 
 client_sockets = set()
-s = socket.socket()
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((host, port))
+s = socket.socket() # Creates a new socket object
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Sets the SO_REUSEADDR in the socket to one
+s.bind((host, port)) # Binds the socket to the address given
 s.listen(5)
 print(f'[*] Listening as {host}:{port}')
 
@@ -16,25 +16,26 @@ print(f'[*] Listening as {host}:{port}')
 def listening(cs):
     while True:
         try:
-            msg = cs.recv(1024).decode()
+            msg = cs.recv(1024).decode() # Receives data from sockets and decodes it
         except Exception as e:
             print(f"[!] Error: {e}")
-            client_sockets.remove(cs)
+            client_sockets.remove(cs)  # If the socket is giving an error, remove it
         else:
             msg = msg.replace(separator_token, ": ")
         for client_socket in client_sockets:
-            client_socket.send(msg.encode())
+            client_socket.send(msg.encode()) # Send the encoded message
 
 while True:
-    client_socket, client_address = s.accept()
+    client_socket, client_address = s.accept() # Waits for an incoming connection and returns the socket and address
     print(f"[+] {client_address} connected.")
     client_sockets.add(client_socket)
 
-    t = Thread(target=listening, args=(client_socket,))
-    t.daemon = True
+    t = Thread(target=listening, args=(client_socket,)) # Creates a Thread object with the target being the listening function
+    t.daemon = True                                     # which takes in the client socket as a parameter
     t.start()
 
 
 for cs in client_sockets:
     cs.close()
+
 s.close()
